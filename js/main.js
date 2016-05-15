df6d6b3cbbc25bdc21f9344d961b2e9d9c6b19dd
Ana Schwendler <schwendler@gmail.com>
2016-05-15 15:49:44 -0300

    menu scrolling :art:

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(".nav a").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+});
+
+function scrollNav() {
+    $('.nav a').click(function(){
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.'+theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - 160
+        }, 400);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
+scrollNav();
